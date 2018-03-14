be04c108fb907a92737415400a521d17e4c02b2e
unknown <hu.lian.cn@gmail.com>
2018-03-14 11:01:08 +0800

    11

--- OUR-KINGDOM/server/controller/RestFramework.js
+++ OUR-KINGDOM/server/controller/RestFramework.js
@@ -6,14 +6,9 @@ var restApp = new Express();
 
 //获取环境变量，配置REST应用
 restApp.use(Express.favicon());
-restApp.use(Express.logger('dev'));
 restApp.use(Express.cookieParser());
 restApp.use(Express.static('client'));
 
-//开发模式使用
-if ('development' === restApp.get('env')) {
-	restApp.use(Express.errorHandler());
-}
 
 //所有API在这里申明
 var authenticationManager = require('../authentication/AuthenticationManager');
