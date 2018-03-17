14632b9b87a4eca9e57e154188851307580d2d3c
unknown <hu.lian.cn@gmail.com>
2018-03-17 15:03:53 +0800

    1

--- OUR-KINGDOM/client/app/GameClientEngine.js
+++ OUR-KINGDOM/client/app/GameClientEngine.js
@@ -9,42 +9,42 @@ define(['socketio','mainloop'],function(socketio,mainloop){
 		//启动gameloop
 		startGameLoop(update,draw);
 		
-		console.log("客户端游戏引擎启动！");
+		window.console.log('客户端游戏引擎启动！');
 	}
 	
 	//接口--游戏引擎关闭
-	function stop(update,draw){
+	function stop(){
 		
 		//关闭gameloop
 		stopGameLoop();
 		
-		console.log("客户端游戏引擎关闭！");
+		window.console.log('客户端游戏引擎关闭！');
 	}
 	
 	
 	function initSocketio(){
 		var token = getCookie('token');
-		console.log("初始化WEBSOCKET,token:"+token)
-		var socket = socketio.connect('http://localhost:3000',{"query":'token='+token});
-		socket.emit('command',{"command":"testDao"},function(data){
-			console.log(data);
+		window.console.log('初始化WEBSOCKET,token:'+token);
+		var socket = socketio.connect('http://localhost:3000',{'query':'token='+token});
+		socket.emit('command',{'command':'testDao'},function(data){
+			window.console.log(data);
 		});
 	}
 	
 	function getCookie(c_name)
 	{
-	if (document.cookie.length>0)
-	  {
-	  c_start=document.cookie.indexOf(c_name + "=")
-	  if (c_start!=-1)
-	    { 
-	    c_start=c_start + c_name.length+1 
-	    c_end=document.cookie.indexOf(";",c_start)
-	    if (c_end==-1) c_end=document.cookie.length
-	    return unescape(document.cookie.substring(c_start,c_end))
-	    } 
-	  }
-	return ""
+		if (document.cookie.length>0)
+		{
+			var c_start=document.cookie.indexOf(c_name + '=');
+			if (c_start!=-1)
+			{ 
+				c_start=c_start + c_name.length+1; 
+				var c_end=document.cookie.indexOf(';',c_start);
+				if (c_end==-1) c_end=document.cookie.length;
+				return unescape(document.cookie.substring(c_start,c_end));
+			} 
+		}
+		return '';
 	}
 	
 	function startGameLoop(update,draw){
